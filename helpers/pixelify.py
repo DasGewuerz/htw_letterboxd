7d86afbaf2fefca17aae29a0b4394102c6f3723f
AnisChaibi <anis.chaibi@wasteer.de>
2024-05-24 11:37:26 +0200

    added pixelation of posters

--- helpers/pixelify.py
+++ helpers/pixelify.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 
 def pixelate(image_path, output_path, pixelation_level):
     image = Image.open(image_path)
@@ -10,19 +11,22 @@ def pixelate(image_path, output_path, pixelation_level):
     result.save(output_path)
 
 def main():
-    image_path = 'poster.jpg'  # Path to your input image
-    output_paths = [
-        'poster_pixelated_1.jpg',
-        'poster_pixelated_2.jpg',
-        'poster_pixelated_3.jpg',
-        'poster_pixelated_4.jpg',
-        'poster_pixelated_5.jpg'
-    ]
-    pixelation_levels = [10, 20, 30, 40, 50]  # Different levels of pixelation
+    folder_path = '/Users/anischaibi/Downloads/development/htw_letterboxd/posters'
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg") and not filename.startswith('pixel'):
+            image_path = f'{folder_path}/{filename}'
+            output_paths = [
+                f'{folder_path}/pixel10_{filename}',
+                f'{folder_path}/pixel20_{filename}',
+                f'{folder_path}/pixel30_{filename}',
+                f'{folder_path}/pixel40_{filename}',
+                f'{folder_path}/pixel50_{filename}'
+            ]
+            pixelation_levels = [10, 20, 30, 40, 50]
 
-    for output_path, level in zip(output_paths, pixelation_levels):
-        pixelate(image_path, output_path, level)
-        print(f'Created {output_path} with pixelation level {level}')
+        for output_path, level in zip(output_paths, pixelation_levels):
+            pixelate(image_path, output_path, level)
+            print(f'Created {output_path} with pixelation level {level}')
 
 if __name__ == '__main__':
     main()
